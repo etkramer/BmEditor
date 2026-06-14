afbd9f8d6d81b1aa7f1d9a5c7fd796f8759780d0
etkramer <elikramergm@gmail.com>
2026-06-13 21:02:23 -0700

    chore: fix fractured static mesh serialization

--- Development/Src/Engine/Src/UnStaticMesh.cpp
+++ Development/Src/Engine/Src/UnStaticMesh.cpp
@@ -2096,7 +2096,7 @@ void UStaticMesh::Serialize(FArchive& Ar)
 	}
 
 #if BATMAN
-	if (!Ar.IsBmCooked(FALSE, FALSE) && !GUseSeekFreeLoading && (!GCookingTarget || !Ar.IsSaving()) && InternalVersion >= 19)
+	if (Ar.IsBmCooked(TRUE) && !GUseSeekFreeLoading && (!GCookingTarget || !Ar.IsSaving()) && InternalVersion >= 19)
 	{
 		Ar << MaterialOverrides;
 	}
